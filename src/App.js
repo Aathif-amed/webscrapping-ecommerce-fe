4b7eb2a38bc56d1c874bf987723c8f639b699635
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2022-11-22 14:30:58 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./fontawesome-free/css/all.css";
+import "./fontawesome-free/js/all.js";
+import "./css/sb-admin-2.min.css";
+import Searchbar from './Searchbar';
+import Sidebar from './Sidebar';
+import Createdata from './Createdata';
+import Dashboard from './Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <>
+ 
+  <BrowserRouter>
+  <div id="wrapper">
+    <Sidebar />
+    <div id="content-wrapper" className="d-flex flex-column">
+      <div id="content">
+
+        <Routes>
+          <Route path="/" element={<Dashboard/>}/>
+          <Route path="/search" element={<Searchbar/>}/>
+          <Route path="/create" element={<Createdata/>}/>
+          {/* <Route path="/createUser" element={<Usercreate/>}/>
+          <Route path="/view/:id" element={<Viewuser/>}/>
+          <Route path="/edit/:id" element={<Edituser/>}/>
+          <Route path="/delete/:id" element={<Deleteuser/>}/> */}
+        </Routes>
+      </div>
     </div>
-  );
+  </div>
+</BrowserRouter>
+</>;
 }
 
 export default App;
